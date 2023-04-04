9bfb37dd4c55c1fdc35fb84d93f56db2b644457d
FlorTiscornia <flora.tiscornia@gmail.com>
2023-04-04 18:37:15 +0200

    advanced

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,27 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import MyView from '../views/MyView.vue'
+import ApiView from '../views/ApiView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/myView',
-      name: 'MyView',
-      component: MyView
+      name: 'ApiView',
+      component: ApiView
     }
   ]
 })
---
+++ vue-project/src/views/ApiView.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>API</h1>
+        <h3></h3>
+        <div class="col3 ">
+    <ul>
+        <li>Presenta BL</li>
+        <li>Cuenta Corriente</li>
+        <li>Reprocesar IB ERROR
+            <a href="#reprocesarIBError" class="scroll-link">
+                <div>
+                    <i class="#"></i>
+                </div>
+                <div>
+                    <span>Reprocesar IB Error</span>
+                    <span>Regenerar error</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+</div>
+    </div>
+</template>
+
+
+
+<style lang="css" scoped>
+
+</style>
