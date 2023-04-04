86cf9e43c5af030c4eeca2c9d9c6cb17f687a95c
FlorTiscornia <flora.tiscornia@gmail.com>
2023-04-04 19:10:35 +0200

    copy information

--- vue-project/src/views/ApiView.vue
+++ vue-project/src/views/ApiView.vue
@@ -7,24 +7,168 @@ export default {
 <template>
     <div>
         <h1>API</h1>
-        <h3></h3>
-        <div class="col3 ">
-    <ul>
-        <li>Presenta BL</li>
-        <li>Cuenta Corriente</li>
-        <li>Reprocesar IB ERROR
-            <a href="#reprocesarIBError" class="scroll-link">
-                <div>
-                    <i class="#"></i>
-                </div>
-                <div>
-                    <span>Reprocesar IB Error</span>
-                    <span>Regenerar error</span>
-                </div>
-            </a>
-        </li>
-    </ul>
-</div>
+            <ul>
+                <li>Reimpresiones
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Por transacción</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Interbanking
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Modificar el cuit ingresado</span>
+                        </div>
+                    </a>
+                </li> 
+                <li>Debines
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Modificar la agenda del pago</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Facturas
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Asociación de NC</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Búsqueda de comprobantes
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>todos los contratantes</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Conformar transacciones
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Aplicación de pago</span>
+                        </div>
+                    </a> 
+                </li>
+                <li>Formulario Toyota
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Creación de transacciones</span>
+                        </div>
+                    </a>
+                </li> 
+                <li>Webservices
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Consultas de LDE</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Créditos validados
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Listado de créditos</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Consulta
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Búsqueda por BL</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Consulta por transacciones
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Búsqueda por numero de transacción</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Retenciones
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Búsqueda de retenciones por usuarios</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Cuenta Corriente
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Regenerar error</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Reprocesar BL
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Escanear originales</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Cuenta Corriente
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Clientes APC</span>
+                        </div>
+                    </a>
+                </li>
+                <li>Reprocesar IB ERROR
+                    <a href="#reprocesarIBError" class="scroll-link">
+                        <div>
+                            <i class="#"></i>
+                        </div>
+                        <div>
+                            <span>Regenerar error</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
     </div>
 </template>
 
